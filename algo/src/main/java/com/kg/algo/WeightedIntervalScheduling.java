ec4c6fb7a34a599d6f7dfd6333bb16a3db351742
Kartheek Nallepalli <kartheek.nallepalli@gmail.com>
2015-11-08 01:58:11 -0700

    update backtrack

--- algo/src/main/java/com/kg/algo/WeightedIntervalScheduling.java
+++ algo/src/main/java/com/kg/algo/WeightedIntervalScheduling.java
@@ -28,41 +28,64 @@ public class WeightedIntervalScheduling {
 		int[] maxWeight = new int[numOfSchedules]; // m[] array
 		
 		maxWeight[0] = weights[0];
-		int[] backTrackArray = new int[numOfSchedules];
-		backTrackArray[0] = -1;
 		int lastChosenSchedule = -1;
 		for(int i=1; i<numOfSchedules; i++){
+			int weight1;
 			if(prevBestInterval[i]>=0){
-				maxWeight[i] = Math.max(weights[i] + maxWeight[prevBestInterval[i]], maxWeight[i-1]);
+				weight1 = weights[i] + maxWeight[prevBestInterval[i]];
 			}else{
-				maxWeight[i] = Math.max(weights[i], maxWeight[i-1]);
+				weight1 = weights[i];
 			}
-			
-			if(prevBestInterval[i] >= 0 && ((weights[i] + maxWeight[prevBestInterval[i]]) > maxWeight[i-1])){
+			int weight2 = maxWeight[i-1];
+			if(weight1 > weight2){
 				lastChosenSchedule = i;
-				backTrackArray[i] = prevBestInterval[i];
-			}else if(weights[i] > maxWeight[i-1]){
-				lastChosenSchedule = i;
-				backTrackArray[i] = -1;
+				maxWeight[i] = weight1;
 			}else{
-				backTrackArray[i] = -1;
+				maxWeight[i] = weight2;
 			}
 		}
 		
 		List<Integer> schedule = new ArrayList<Integer>();
-		while(lastChosenSchedule != -1){
-			schedule.add(lastChosenSchedule);
-			//System.out.println(lastChosenSchedule);
-			lastChosenSchedule = backTrackArray[lastChosenSchedule];
-		}
+		findSolution(lastChosenSchedule, weights, prevBestInterval, maxWeight, schedule);
+		
 		return schedule;
 	}
 	
-	
+	private static void findSolution(int lastChosenSchedule, int[] weights,
+			int[] prevBestInterval, int[] maxWeight, List<Integer> schedule) {
+		
+		if(lastChosenSchedule==0){
+			schedule.add(lastChosenSchedule);
+			return;
+		}
+
+		int flag = 0;
+		int weight1;
+		if(prevBestInterval[lastChosenSchedule]!=-1){
+			weight1 = weights[lastChosenSchedule] + maxWeight[prevBestInterval[lastChosenSchedule]];
+		}else{
+			weight1 = weights[lastChosenSchedule];
+			flag = 1;
+		}
+		if(weight1 > maxWeight[lastChosenSchedule-1]){
+			schedule.add(lastChosenSchedule);
+			if(flag==1) return;
+			findSolution(prevBestInterval[lastChosenSchedule], weights, prevBestInterval, maxWeight, schedule);
+		}else{
+			findSolution(lastChosenSchedule-1, weights, prevBestInterval, maxWeight, schedule);
+		}
+	}
+
+
 	public static void main(String args[]){
-		int[] startTimes = {1, 2, 5, 2, 9, 10};
-		int[] endTimes = {4, 6, 7, 11, 12, 13};
-		int[] weights = {2, 4, 4, 7, 2, 1};
+//		int[] startTimes = {1, 2, 5, 2, 9, 10};
+//		int[] endTimes = {4, 6, 7, 11, 12, 13};
+//		int[] weights = {2, 4, 4, 7, 2, 1};
+		
+		int[] startTimes = {1, 2, 6}; 
+		int[] endTimes = {3, 5, 8};
+		int[] weights = {2, 1, 6};
+		
 		System.out.println(getBestSchedule(startTimes, endTimes, weights));
 	}
 }
